c04112c99adad288504f3b53944b16e768401736
Saul Alonso Monsalve <saul.alonso.monsalve@cern.ch>
2025-09-19 15:27:45 +0200

    minor change

--- pretrain_fix.sh
+++ pretrain_fix.sh
@@ -17,24 +17,24 @@ dropout_dec=0.0
 attn_dropout_dec=0.0
 drop_path_rate_dec=0.0
 mask_ratio=0.5
-epochs=400
+epochs=5
 num_workers=16
 blr=1.5e-4
-accum_grad_batches=2
-warmup_epochs=40
-cosine_annealing_epochs=360
+accum_grad_batches=1
+warmup_epochs=1
+cosine_annealing_epochs=4
 weight_decay=0.05
 beta1=0.9
 beta2=0.95
 save_dir="logs_final"
-name="pretrain_v5.1b_dlnu_log_base_v17"
+name="pretrain_v5.1b_dlnu_diagnose2"
 log_every_n_steps=50
 save_top_k=1
 checkpoint_path="checkpoints_final"
-checkpoint_name="pretrain_v5.1b_dlnu_log_base_v17"
+checkpoint_name="pretrain_v5.1b_dlnu_diagnose2"
 early_stop_patience=200
 load_checkpoint="checkpoints_final/mae_v5.1b_noglob_v12/loss_val_total/last.ckpt"
-gpus=(1)
+gpus=(0 1)
 
 python -m train.pretrain \
     --train \
