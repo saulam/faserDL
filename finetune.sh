1c05c75282aac9f6fa5372437eec97f9ce94f2c9
Saul Alonso Monsalve <saul.alonso.monsalve@cern.ch>
2025-09-30 15:08:37 +0200

    minor changes

--- finetune.sh
+++ finetune.sh
@@ -28,13 +28,13 @@ beta2=0.999
 ema_decay=0.9999
 head_init=2e-5
 save_dir="logs_final"
-name="finetune_v5.1b_dlnu_log_base_clariden_0_5_v37"
+name="finetune_v5.1b_dlnu_log_base_clariden_0_5_v38"
 log_every_n_steps=10
 save_top_k=1
 checkpoint_path="checkpoints_final"
-checkpoint_name="finetune_v5.1b_dlnu_log_base_clariden_0_5_v37"
+checkpoint_name="finetune_v5.1b_dlnu_log_base_clariden_0_5_v38"
 early_stop_patience=10
-load_checkpoint="checkpoints_final/pretrain_v5.1b_dlnu_log_base_clariden_0_5_lowlr_v18/loss_total_val/epoch=103-step=37440.ckpt"
+load_checkpoint="checkpoints_final/pretrain_v5.1b_dlnu_log_base_clariden_0_5_lowlr_v18/loss_total_val/epoch=243-step=87840.ckpt"
 gpus=(0 1)
 
 python -m train.finetune \
